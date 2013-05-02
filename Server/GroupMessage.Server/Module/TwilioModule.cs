ae2298b4e7aafd305f9b93f7cde1d74bdd76854a
Jakob Færch <jrf@trifork.com>
2013-05-03 00:15:37 +0200

    Body from Twilio requests is extracted and messages are sent

--- Server/GroupMessage.Server/Module/TwilioModule.cs
+++ Server/GroupMessage.Server/Module/TwilioModule.cs
@@ -7,16 +7,37 @@ using System.IO;
 using MongoDB.Driver.Builders;
 using System;
 using Nancy;
+using Nancy.Helpers;
 
 namespace GroupMessage.Server.Module
 {
+    /// <summary>
+    /// Receives requests from Twilio when group members responds to sms messages
+    /// </summary>
     public class TwilioModule : ModuleBase
     {
         public TwilioModule(MessageService _messageService) : base("twilio")
         {
             Post ["/sms"] = parameters =>
             {
-                Console.WriteLine("Received from Twilio: " + Request.Body.GetAsString());
+                var bodyEntity = Request.Body.GetAsString ();
+                var map = HttpUtility.ParseQueryString (bodyEntity);
+                Console.WriteLine("Received from Twilio: " + bodyEntity);
+                var smsBody = map["Body"];
+                Console.WriteLine("Extracted message body: " + smsBody);
+
+                var status = map ["SmsStatus"];
+                if (status == "received")
+                {
+                    var message = new Message{MessageId=Guid.NewGuid().ToString(), Text=smsBody };
+                    _messageService.initialSend(message);
+                }
+                else
+                {
+                    Console.WriteLine("Ignoring unknown status '" + status + "'");
+                }
+
+
                 return new Response().SetContentType("application/xml").SetBody("<?xml version='1.0' encoding='UTF-8'?><Response></Response>"); // I don't know why, but Twilio requires a Response document in order to acknowledge the receive
             };
         }
