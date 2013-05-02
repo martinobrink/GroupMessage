3c2185c57fe733acaf07758492ff26d683d72f4e
Simon Hem Pedersen <shp@trifork.com>
2013-05-03 00:52:38 +0200

    code cleanup

--- Server/GroupMessage.Server.Test/Module/BrowserExtensions.cs
+++ Server/GroupMessage.Server.Test/Module/BrowserExtensions.cs
@@ -36,11 +36,8 @@
         public static Nancy.Testing.BrowserResponse Delete(this Nancy.Testing.Browser browser, string route)
         {
             return browser.Delete(route, with =>
-                               {
+            {
                 with.HttpRequest();
-/*                with.Header("accept", "application/json");
-                with.Header("Content-Type", "application/json");
-                with.Body(bodyContent);*/
             });
         }
     }
