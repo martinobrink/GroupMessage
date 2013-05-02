8335a3b5e0b1ea69fbd7d6de0c2b24ede8ba163e
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-02 18:24:36 +0200

    Removed Mongo ObjectId from json serialization

--- Server/GroupMessage.Server/Model/EntityBase.cs
+++ Server/GroupMessage.Server/Model/EntityBase.cs
@@ -1,11 +1,13 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Nancy.Json;
 
 namespace GroupMessage.Server.Model
 {
     public abstract class EntityBase : IEntity
     {
         [BsonId]
+        [ScriptIgnore]
         public ObjectId Id { get; set; }
     }
 }
