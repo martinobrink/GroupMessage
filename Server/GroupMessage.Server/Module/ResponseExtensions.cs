30be8330cd1bd8fa0cce639fd768f092530b36ea
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-02 13:15:55 +0200

    Added Extension method to set body on response

---
+++ Server/GroupMessage.Server/Module/ResponseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Nancy;
+
+namespace GroupMessage.Server.Module
+{
+    public static class ResponseExtensions
+    {
+        public static Response SetBody(this Response response, string body)
+        {
+            response.Contents = s =>
+                {
+                    var streamWriter = new StreamWriter(s);
+                    streamWriter.Write(body);
+                    streamWriter.Flush();
+                };
+
+            return response;
+        }
+    }
+}
