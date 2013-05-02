ae64665f15b94489dc89b3d963d0dc434f9bdf0f
Jakob Færch <jrf@trifork.com>
2013-05-02 22:34:28 +0200

    Added redirect from / to /index.html in the webapp

---
+++ Server/GroupMessage.Server/Module/StaticModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+
+namespace GroupMessage.Server.Module
+{
+    /// <summary>
+    /// Takes care of redirecting from the root URL to the admin gui
+    /// </summary>
+    public class StaticModule : ModuleBase
+    {
+        public StaticModule() : base("")
+        {
+            Get ["/"] = parameters =>
+            {
+                return Response.AsRedirect("index.html");
+            };
+        }
+    }
+}
