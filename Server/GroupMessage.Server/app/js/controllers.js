4e0f54dc6e0ff03ca383b20951a80df38146b100
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-10 12:06:53 +0200

    Added controller AppVersionCtrl to web app for easy displaying of the current version running

--- Server/GroupMessage.Server/app/js/controllers.js
+++ Server/GroupMessage.Server/app/js/controllers.js
@@ -14,13 +14,12 @@ function MessagesViewCtrl($scope, $http) {
     $scope.lastForm = {};
 
     $scope.sendMessage = function(textMessage) {
-        var messageId = "messageId"+new Date().getTime();
-        var url = "/groupmessage/message/"+messageId;
-        var message = {MessageId:messageId, Text:textMessage};
+        var messageId = "messageId" + new Date().getTime();
+        var url = "/groupmessage/message/" + messageId;
+        var message = { MessageId: messageId, Text: textMessage };
 
         $http.put(url, message);
-    }
-
+    };
 }
 
 UsersViewCtrl.$inject = ['$scope', '$http'];
@@ -30,29 +29,29 @@ function UsersViewCtrl($scope, $http) {
     $scope.lastForm = {};
 
     $scope.init = function () {
-        $http.get("/groupmessage/user/").then(function(result){
+        $http.get("/groupmessage/user/").then(function(result) {
             var data = result.data;
             $scope.users = data;
-        })
+        });
     };
 
     $scope.deleteUser = function(phoneNumber) {
-        $http.delete("/groupmessage/user/"+phoneNumber).then(function(){
+        $http.delete("/groupmessage/user/" + phoneNumber).then(function() {
             $scope.user = null;
             $scope.init();
-        })
-    }
+        });
+    };
 
     $scope.update = function(user) {
-        $http.put("/groupmessage/user/", JSON.stringify($scope.user)).then(function(){
+        $http.put("/groupmessage/user/", JSON.stringify($scope.user)).then(function() {
             $scope.user = null;
             $scope.init();
-        })
-    }
+        });
+    };
 
     $scope.editUser = function(user) {
         $scope.user = user;
-    }
+    };
 }
 
 StatusViewCtrl.$inject = ['$scope', '$http'];
@@ -62,9 +61,18 @@ function StatusViewCtrl($scope, $http) {
     $scope.lastForm = {};
 
     $scope.init = function () {
-        $http.get("/groupmessage/transaction/").then(function(result){
+        $http.get("/groupmessage/transaction/").then(function(result) {
             var data = result.data;
             $scope.statuses = data;
-        })
+        });
     };
+}
+
+AppVersionCtrl.$inject = ['$scope', '$http'];
+
+function AppVersionCtrl($scope, $http) {
+
+    $http.get("/groupmessage/version/").success(function (data) {
+        $scope.appVersion = data;
+    });
 }
--- Server/GroupMessage.Server/app/js/services.js
+++ Server/GroupMessage.Server/app/js/services.js
@@ -7,9 +7,3 @@
 var services = angular.module('myApp.services', []);
 
 services.value('version', '0.2');
-
-
-
-
-
-
