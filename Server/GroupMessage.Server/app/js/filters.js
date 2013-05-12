2b4ec047d9874600f5e4e00dc948d64be4ebe352
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-12 21:15:33 +0200

    Some javascript wiggling in order to get datetime display of LastUpdate correct on website

--- Server/GroupMessage.Server/app/js/filters.js
+++ Server/GroupMessage.Server/app/js/filters.js
@@ -5,7 +5,14 @@
 angular.module('myApp.filters', [])
     .filter('displayDate', function () {
         return function (dateString) {
-            return new Date(dateString).toLocaleString('da-DK');
+            var date = new Date(dateString);
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;//seems like a weird bug in ecmascript datetime handling??
+            var day = date.getDate();
+            var hours = ('0' + date.getHours()).slice(-2);//'0'+slice: zero-pad if necessary
+            var minutes = ('0' + date.getMinutes()).slice(-2);//'0'+slice: zero-pad if necessary
+            var seconds = ('0' + date.getSeconds()).slice(-2);//'0'+slice: zero-pad if necessary
+            return day + '/' + month + '-' + year + ' ' + hours + ':' + minutes + ':' + seconds;
         };
     })
     .filter('deviceImage', function () {
