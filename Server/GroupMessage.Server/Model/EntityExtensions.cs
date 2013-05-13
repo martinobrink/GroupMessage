ca83146265e3cec576a289c4ba1005e914a6fd6b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-13 20:08:23 +0200

    Replaced default serializer used for serializing entities with Json.NET

--- Server/GroupMessage.Server/Model/EntityExtensions.cs
+++ Server/GroupMessage.Server/Model/EntityExtensions.cs
@@ -1,11 +1,12 @@
-﻿namespace GroupMessage.Server.Model
+﻿using Newtonsoft.Json;
+
+namespace GroupMessage.Server.Model
 {
     public static class EntityExtensions
     {
         public static string AsJson<TEntity>(this TEntity entity) where TEntity : EntityBase
         {
-            var serializer = new Nancy.Json.JavaScriptSerializer();
-            return serializer.Serialize(entity);
+            return JsonConvert.SerializeObject(entity);
         }
     }
 }
