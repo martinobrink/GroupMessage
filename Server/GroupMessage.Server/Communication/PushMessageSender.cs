fb905973408f1211632c631297751452ef481c17
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-14 21:55:24 +0200

    Added handling of other device types to PushMessageSender so that delivery fails with an error message.

--- Server/GroupMessage.Server/Communication/PushMessageSender.cs
+++ Server/GroupMessage.Server/Communication/PushMessageSender.cs
@@ -41,21 +41,50 @@ namespace GroupMessage.Server.Communication
 
         public SendStatus Send(User user, string text)
         {
-            string jsonData = @"{ ""registration_ids"": ["""+user.DeviceToken+"\"], \"data\":  { \"Message\": \""+text+"\", \"Group\": \"All\" } }";
+            switch (user.DeviceOs)
+            {
+                case DeviceOs.Android:
+                    {
+                        return SendGooglePushNotification(user, text);
+                    }
+                case DeviceOs.iOS:
+                    {
+                        return new SendStatus { Success = false, ErrorMessage = "Devices connected via iOS not supported yet..." };
+                    }
+                case DeviceOs.WindowsPhone:
+                    {
+                        return new SendStatus { Success = false, ErrorMessage = "Devices connected via Windows Phone not supported yet..." };
+                    }
+                case DeviceOs.Windows8:
+                    {
+                        return new SendStatus { Success = false, ErrorMessage = "Devices connected via Windows 8 not supported yet..." };
+                    }
+                case DeviceOs.NotSet:
+                    {
+                        return new SendStatus {Success = false, ErrorMessage = "No device connected via app"};
+                    }
+            }
+
+            return new SendStatus { Success = false, ErrorMessage = string.Format("Unknown value '{0}' of user.DeviceOs. Unable to send message via push notification.", user.DeviceOs.ToString()) };
+        }
+
+        private SendStatus SendGooglePushNotification(User user, string text)
+        {
+            string jsonData = @"{ ""registration_ids"": [""" + user.DeviceToken + "\"], \"data\":  { \"Message\": \"" + text + "\", \"Group\": \"All\" } }";
             var httpClient = new HttpClient();
             var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://android.googleapis.com/gcm/send");
             requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             requestMessage.Headers.Authorization = new AuthenticationHeaderValue("key", string.Format("={0}", _googleApiKey));
             requestMessage.Content = new StringContent(jsonData);
             requestMessage.Content.Headers.ContentType.MediaType = "application/json";
-            
+
             var responseMessage = httpClient.SendAsync(requestMessage).Result;
             var result = responseMessage.Content.ReadAsStringAsync().Result;
             var pushNotificationResult = JsonConvert.DeserializeObject<GooglePushNotificationResult>(result);
-            
+
             return new SendStatus
                 {
-                    Success = pushNotificationResult.success, 
+                    Success = pushNotificationResult.success,
                     ErrorMessage = pushNotificationResult.results[0].error
                 };
         }
