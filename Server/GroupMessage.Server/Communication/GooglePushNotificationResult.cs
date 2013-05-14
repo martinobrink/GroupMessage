a2425ec2b02d92fae58b7387e36e776621bf278b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-05-14 21:01:04 +0200

    Added support for Android push notifications!

---
+++ Server/GroupMessage.Server/Communication/GooglePushNotificationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace GroupMessage.Server.Communication
+{
+    public class GooglePushNotificationResult
+    {
+        public long multicast_id { get; set; }
+        public bool success { get; set; }
+        public bool failure { get; set; }
+        public int canonical_ids { get; set; }
+        public Result[] results { get; set; }
+    }
+
+    public class Result
+    {
+        public string message_id { get; set; }
+        public string error { get; set; }
+    }
+}
--- Server/GroupMessage.Server/Communication/PushMessageSender.cs
+++ Server/GroupMessage.Server/Communication/PushMessageSender.cs
@@ -1,17 +1,63 @@
-﻿using GroupMessage.Server.Model;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using GroupMessage.Server.Model;
+using Newtonsoft.Json;
 
 namespace GroupMessage.Server.Communication
 {
     public class PushMessageSender : IMessageSender
     {
+        private string _googleApiKey;
+        private string _googleSenderId;
+        private string _googlePackageName;
+
         public MessageSenderType SenderType
         {
             get { return MessageSenderType.PushNotification; }
         }
 
+        public PushMessageSender()
+        {
+            List<string> googlePushConfigLines;
+            try
+            {
+                googlePushConfigLines = File.ReadLines("GooglePushNotifications.txt").ToList();
+            }
+            catch (Exception exception)
+            {
+                var errorMessage = string.Format("Something failed during reading of file GooglePushNotifications.txt! Did you forget to add a GooglePushNotifications.txt to the same directory as the application? Exception: {0}", exception.Message);
+                Console.WriteLine(errorMessage);
+                throw new InvalidOperationException(errorMessage);
+            }
+
+            _googleApiKey = googlePushConfigLines[0].Split('=')[1];
+            _googleSenderId = googlePushConfigLines[1].Split('=')[1];
+            _googlePackageName = googlePushConfigLines[2].Split('=')[1];
+        }
+
         public SendStatus Send(User user, string text)
         {
-            return new SendStatus{Success = true, NumberOfTries = 1, ErrorMessage = null};
+            string jsonData = @"{ ""registration_ids"": ["""+user.DeviceToken+"\"], \"data\":  { \"Message\": \""+text+"\", \"Group\": \"All\" } }";
+            var httpClient = new HttpClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://android.googleapis.com/gcm/send");
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("key", string.Format("={0}", _googleApiKey));
+            requestMessage.Content = new StringContent(jsonData);
+            requestMessage.Content.Headers.ContentType.MediaType = "application/json";
+            
+            var responseMessage = httpClient.SendAsync(requestMessage).Result;
+            var result = responseMessage.Content.ReadAsStringAsync().Result;
+            var pushNotificationResult = JsonConvert.DeserializeObject<GooglePushNotificationResult>(result);
+            
+            return new SendStatus
+                {
+                    Success = pushNotificationResult.success, 
+                    ErrorMessage = pushNotificationResult.results[0].error
+                };
         }
     }
 }
